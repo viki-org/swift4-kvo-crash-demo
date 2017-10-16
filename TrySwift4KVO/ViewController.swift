bb7b38574f5640966a209651ef11e89e5b05dc6c
Thongchai Kolyutsakul <thongchai.kolyutsakul@viki.com>
2017-10-16 16:13:44 +0800

    Initial Commit

---
+++ TrySwift4KVO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrySwift4KVO
+//
+//  Created by Thongchai Kolyutsakul on 16/10/17.
+//  Copyright © 2017 Hlung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
